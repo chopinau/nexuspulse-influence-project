ca65770d516e4a8d7e33427dd216c1cc7d3b6a00
chopinau <chopinau@gmail.com>
2026-02-10 15:59:16 +0800

    54

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Other config options here
+    // Enable tree shaking for better optimization
+    productionBrowserSourceMaps: false
 };
 
 export default withNextIntl(nextConfig);
